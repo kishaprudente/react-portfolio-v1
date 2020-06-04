f359b564abcbab3f885155774e482726158ead6b
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-03 21:31:17 -0400

    added links for pages to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { Anchor, Box, Main, Nav } from "grommet";
+import React from "react";
+import { Anchor, Nav } from "grommet";
+import { Link } from "react-router-dom";
 
 const navItems = [
-  { label: "HOME", href: "#" },
-  { label: "PROJECTS", href: "#" },
-  { label: "CONTACT", href: "#" },
+	{ label: "about", href: "/" },
+	{ label: "projects", href: "/projects" },
+	{ label: "contact", href: "/contact" },
 ];
 
 const NavBar = () => (
-    <Box background="dark-1" pad="large" fill>
-      <Box direction="row" pad={{ vertical: "medium" }}>
-        <Nav width="small" margin={{ right: "large" }}>
-          {navItems.map(item => (
-            <Anchor href={item.href} label={item.label} key={item.label} />
-          ))}
-        </Nav>
-        <Main>Place main content here</Main>
-      </Box>
-    </Box>
+	<Nav direction="row" align="right">
+		{navItems.map((item) => (
+			<Anchor href={item.href} label={item.label} key={item.label}>
+				<Link to={item.href} />
+			</Anchor>
+		))}
+	</Nav>
 );
 
 export default NavBar;
