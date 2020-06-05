c7a0cda8240135398fd7eed797daafb635d84d74
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-05 12:55:23 -0400

    created seeds for projects

---
+++ src/utils/projects.js
@@ -0,0 +1,34 @@
+const projects = [
+    {
+        title: "Touristee",
+        repo: "https://github.com/kishaprudente/touristee",
+        demo: "https://kishaprudente.github.io/touristee/",
+        image: "../../assets/touristee.png"
+    },
+    {
+        title: "Eat Da Burgers!",
+        repo: "https://github.com/kishaprudente/eat-da-burger",
+        demo: "https://polar-dusk-74766.herokuapp.com/",
+        image: "../../assets/eat-da-burgers.png"
+    },
+    {
+        title: "Employee Summary",
+        repo: "https://github.com/kishaprudente/employee-summary",
+        demo: "https://kishaprudente.github.io/employee-summary/",
+        image: "../../assets/employee-summary.png"
+    },
+    {
+        title: "Weather Dashboard",
+        repo: "https://github.com/kishaprudente/weather-dashboard",
+        demo: "https://kishaprudente.github.io/weather-dashboard/",
+        image: "../../assets/weather-dashboard.png"
+    },
+    {
+        title: "Day Scheduler",
+        repo: "https://github.com/kishaprudente/day-scheduler",
+        demo: "https://kishaprudente.github.io/day-scheduler/",
+        image: "../../assets/day-scheduler.png"
+    },
+]
+
+export default projects;
