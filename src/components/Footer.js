643a982eecbbe0b1a04c9abbae4dc1482f3ea965
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-07 15:15:04 -0400

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer, Text } from "grommet";
+
+const FooterComponent = () => {
+    return(
+        <Footer background="light-1" justify="center" pad="small">
+            <Text textAlign="center" size="small">
+            With ♡ Kisha Prudente 2020
+            </Text>
+        </Footer>
+    )
+}
+
+export default FooterComponent;
