abb57843cdbb24e4bb98bbbf7b5b606a29f39e68
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-07 15:15:43 -0400

    added links for nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,43 @@
 import React from "react";
-import { Anchor, Nav } from "grommet";
+import { Header, Nav, Box, Anchor } from "grommet";
 import { Link } from "react-router-dom";
 
 const navItems = [
-	{ label: "about", href: "/" },
+	{ label: "about", href: "/about" },
 	{ label: "projects", href: "/projects" },
 	{ label: "contact", href: "/contact" },
+	{ label: "skills", href: "/skills" },
 ];
 
-const NavBar = () => (
-	<Nav direction="row">
-		{navItems.map((item) => (
-			<Anchor href={item.href} label={item.label} key={item.label}>
-				<Link to={item.href} />
-			</Anchor>
-		))}
-	</Nav>
-);
+export function HeaderContainer({children}) {
+	return(
+		<Header pad="medium" style={styles.header}>
+			{children}
+		</Header>
+	)
+}
+export function NavBar() {
+	// const location = useLocation();
+	return(
+		<Header pad="medium" style={styles.header}>
+			<Box direction="row" gap="medium">
+				<Anchor color="black" href="/">
+				Kisha Prudente
+				</Anchor>
+			</Box>
+			<Nav direction="row">
+				{navItems.map(item => (
+				<Link style={styles.link} to={item.href} key={item.label}>{item.label}</Link>
+				))}
+			</Nav>
+		</Header>
+	)
+}
 
-export default NavBar;
+const styles = {
+	link: {
+		textDecoration: "none",
+		color: "black",
+
+	},
+};
