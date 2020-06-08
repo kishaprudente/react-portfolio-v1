5d6431984f22d9283e066492ee934b3076e94a59
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-08 09:32:56 -0400

    fix footer alignment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Footer, Text } from "grommet";
 
 const FooterComponent = () => {
     return(
-        <Footer background="light-1" justify="center" pad="small">
+        <Footer background="light-1" justify="center" pad="large">
             <Text textAlign="center" size="small">
             With ♡ Kisha Prudente 2020
             </Text>
