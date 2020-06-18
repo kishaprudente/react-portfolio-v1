0c418a4f29275d18624f4d22fb05488055056b60
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-18 18:07:01 -0400

    removed unused styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import { Grommet, Main, ResponsiveContext } from "grommet";
+import { Grommet, Main } from "grommet";
 import { dark } from "grommet/themes";
 import { deepMerge } from "grommet/utils";
 
@@ -14,15 +14,12 @@ import Skills from "./pages/Skills";
 import { NavBar } from "./components/NavBar";
 import FooterComponent from "./components/Footer";
 
-const { Consumer } = ResponsiveContext;
-
 function App() {
 	return (
 		<Router>
 			<Grommet background="white" full theme={customTheme}>
 				<NavBar />
 				<Main
-					align="center"
 					border={{ side: "top", size: "small", color: "black" }}
 				>
 					<Route exact path="/" component={Home} />
@@ -48,35 +45,7 @@ const customTheme = deepMerge(dark, {
 			color: "white",
 		},
 		breakpoints: {
-			small: {
-				value: 768,
-				borderSize: {
-					xsmall: "1px",
-					small: "2px",
-					medium: "4px",
-					large: "6px",
-					xlarge: "12px",
-				},
-				edgeSize: {
-					none: "0px",
-					hair: "1px",
-					xxsmall: "2px",
-					xsmall: "3px",
-					small: "6px",
-					medium: "12px",
-					large: "24px",
-					xlarge: "48px",
-				},
-				size: {
-					xxsmall: "24px",
-					xsmall: "48px",
-					small: "96px",
-					medium: "192px",
-					large: "384px",
-					xlarge: "768px",
-					full: "100%",
-				},
-			},
+			small: { value: 768 },
 			medium: { value: 1536 },
 			large: {},
 		},
