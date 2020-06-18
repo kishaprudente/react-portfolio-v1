915506bef15b76c0d2c8555b6c776f24a383cf63
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-18 18:05:52 -0400

    changed padding size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import { Footer, Text } from "grommet";
 
 const FooterComponent = () => {
     return(
-        <Footer background="light-1" justify="center" pad="large">
+        <Footer background="light-1" justify="center" pad="medium">
             <Text textAlign="center" size="small">
-            With ♡ Kisha Prudente 2020
+            ♡ Kisha Prudente | 2020
             </Text>
         </Footer>
     )
