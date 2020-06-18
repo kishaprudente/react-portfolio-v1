e7ac36f01d7e925df584f1d7003828aa2e1492ae
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-18 18:08:16 -0400

    added about page to home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "grommet";
+import About from "./About";
+
+const Home = () => {
+    return(
+        <div>
+            <About />
+        </div>
+    )
+}
+
+export default Home;
