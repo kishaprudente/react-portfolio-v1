c5f785e6ee2c5e5847f5a6ab9557c3816deeaba3
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-19 00:09:49 -0400

    added background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,84 @@
 import React from "react";
-import { Box } from "grommet";
+import styled from 'styled-components';
+import { Box, Text, Image, Anchor } from "grommet";
+import { Facebook, Linkedin, Github, Instagram, Mail } from 'grommet-icons';
 import About from "./About";
+import myPhoto from '../assets/my-photo.jpg';
+import flowerbg from '../assets/flowerbg.jpg';
+
+const StyledLinks = styled(Box)`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    font-size: 25px;
+`;
+
+const Hero = styled(Box)`
+    display: flex;
+    alignContent: center;
+    padding: 2em;
+    justify: center;
+    background-image: url(${flowerbg});
+    background-attachment: fixed;
+    width: 100%;
+    height: 70%;
+    background-size: cover;
+`;
+
+const Title = styled(Text)`
+    text-align: center;
+    font-size: 25px;
+    font-family: Karla;
+    font-weight: Bolder;
+    text-shadow: 2px 2px darkgrey;
+`;
+
+const MyPhoto = styled(Image)`
+    border-radius: 100px;
+    width: 200px;
+    height: 200px;
+    display: flex;
+    align-self: center;
+`;
 
 const Home = () => {
     return(
         <div>
+            <Hero gap='medium'>
+                <MyPhoto opacity='strong' src={myPhoto} />
+                <Title>
+                <h2>Software Engineer</h2>
+                <h2>Full Stack Web Developer</h2>
+                </Title>
+                <StyledLinks>
+                    <Anchor icon={<Linkedin size='medium'/>} href='https://www.linkedin.com/in/kishalozada/'/>
+                    <Anchor icon={<Github />} href='https://github.com/kishaprudente'/>
+                    <Anchor icon={<Instagram />} href='https://www.instagram.com/kishamalzd/'/>
+                    <Anchor icon={<Facebook />} href='https://www.facebook.com/kishaax'/>
+                    <Anchor icon={<Mail />} href='mailto:kisha.prudente@gmail.com'/>
+                </StyledLinks>
+            </Hero>
             <About />
         </div>
     )
 }
 
 export default Home;
+
+const styles = {
+      image: {
+        borderRadius: '100px',
+        width: '200px',
+        height: '200px',
+        display: 'flex',
+        alignSelf: 'center',
+      },
+      text: {
+        alignSelf: 'center',
+        fontSize: '25px',
+        fontFamily: 'KarlaBold',
+        fontWeight: 'Bolder',
+        color: 'black',
+        textShadow: '2px 2px darkgrey',
+      },
+}
