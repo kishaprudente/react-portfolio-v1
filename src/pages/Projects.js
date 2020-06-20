5e1cdef2cb31c25ee447fee795474e7cd9c774de
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-20 10:21:34 -0400

    created starter project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from 'styled-components';
+import { Box } from 'grommet';
+
+const Container = styled(Box)`
+    display: flex,
+`;
+
+const Title = styled.h1`
+  padding: 0.2em
+`;
 
 const Projects = () => {
     return(
-        <div>
-            This is the projects page.
-        </div>
+        <Container gap='medium' pad='medium'>
+            <Title>02. Projects</Title>
+        </Container>
     )
 }
 
