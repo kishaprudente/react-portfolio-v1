a6a58dc37215fe62523ec62dd29c38a12dc0af45
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-22 23:49:01 -0400

    refactored home and about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Box, Text, Image } from 'grommet';
-import KPLogo from '../assets/KPlogo.png'
-// import myPhoto from '../assets/my-photo.jpg';
+import { Box, Image } from 'grommet';
+import KPLogo from '../assets/KPlogo.png';
+import PageTitle from '../components/PageTitle';
 
 const Container = styled(Box)`
   display: flex;
   justify-content: center;
   padding: 1.5em;
-`;
-
-const StyledText = styled(Text)`
-  align-self: center;
-  font-size: 23px;
-  font-Family: Karla;
-`;
-
-const Title = styled.h1`
-  padding: 0.2em
 `;
 
 const Body = styled(Box)`
@@ -37,21 +27,19 @@ const Logo = styled(Image)`
 
 const About = () => {
   return (
-      <Container align='center' >
-        <StyledText>
-          <Title>01. ABOUT ME</Title>
-  
-          <Body>
-          <p>Hi, My name is Kisha Prudente.</p>
-          <p>
-            Currently based in New York City.  I graduated with a
-            Bachelor's in Software Engineering in the Philippines. I like to eat, sleep and code. I aim to develop high quality software, mobile, and web application
-            with clean, elegant and efficient code.
-          </p>
-          <Logo src={KPLogo} />
-          </Body>
-        </StyledText>
-      </Container>
+    <Container align='center'>
+      <PageTitle>ABOUT ME</PageTitle>
+      <Body>
+        <p>Hi, My name is Kisha Prudente.</p>
+        <p>
+          Currently based in New York City. I graduated with a Bachelor's in
+          Software Engineering in the Philippines. I like to eat, sleep and
+          code. I aim to develop high quality software, mobile, and web
+          application with clean, elegant and efficient code.
+        </p>
+        <Logo src={KPLogo} />
+      </Body>
+    </Container>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,74 +1,86 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
-import { Box, Text, Image, Anchor, Button } from "grommet";
+import { Link } from 'react-router-dom';
+import { Box, Text, Image, Anchor, Button } from 'grommet';
 import { Facebook, Linkedin, Github, Instagram, Mail } from 'grommet-icons';
-import About from "./About";
+import About from './About';
 import Projects from './Projects';
 import myPhoto from '../assets/my-photo.jpg';
 import flowerbg from '../assets/flowerbg.jpg';
 
 const StyledLinks = styled(Box)`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    font-size: 25px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  font-size: 25px;
 `;
 
 const Hero = styled(Box)`
-    display: flex;
-    alignContent: center;
-    padding: 2em;
-    justify: center;
-    background-image: url(${flowerbg});
-    background-attachment: fixed;
-    width: 100%;
-    height: 70%;
-    background-size: cover;
+  display: flex;
+  aligncontent: center;
+  padding: 2em;
+  justify: center;
+  background-image: url(${flowerbg});
+  background-attachment: fixed;
+  width: 100%;
+  background-size: cover;
 `;
 
 const Title = styled(Text)`
-    text-align: center;
-    font-size: 25px;
-    font-family: Karla;
-    font-weight: Bolder;
-    text-shadow: 2px 2px darkgrey;
+  text-align: center;
+  font-size: 25px;
+  font-family: Karla;
+  font-weight: Bolder;
+  text-shadow: 2px 2px darkgrey;
 `;
 
 const MyPhoto = styled(Image)`
-    border-radius: 100px;
-    width: 200px;
-    height: 200px;
-    display: flex;
-    align-self: center;
+  border-radius: 100px;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  align-self: center;
 `;
 
 const ProjectButton = styled(Button)`
-    text-align: center;
-    align-self: center;
+  text-align: center;
+  align-self: center;
+  text-decoration: none;
+  padding: 5px;
 `;
 
 const Home = () => {
-    return(
-        <div>
-            <Hero gap='medium'>
-                <MyPhoto opacity='strong' src={myPhoto} />
-                <Title>
-                <h2>Software Engineer</h2>
-                <h2>Full Stack Web Developer</h2>
-                </Title>
-                <StyledLinks>
-                    <Anchor icon={<Linkedin size='medium'/>} href='https://www.linkedin.com/in/kishalozada/'/>
-                    <Anchor icon={<Github />} href='https://github.com/kishaprudente'/>
-                    <Anchor icon={<Instagram />} href='https://www.instagram.com/kishamalzd/'/>
-                    <Anchor icon={<Facebook />} href='https://www.facebook.com/kishaax'/>
-                    <Anchor icon={<Mail />} href='mailto:kisha.prudente@gmail.com'/>
-                </StyledLinks>
-                <ProjectButton size='medium' label='See My Work' path='/projects'/>
-            </Hero>
-            <About />
-            <Projects />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Hero gap='medium'>
+        <MyPhoto opacity='strong' src={myPhoto} />
+        <Title>
+          <h2>Software Engineer</h2>
+          <h2>Full Stack Web Developer</h2>
+        </Title>
+        <StyledLinks>
+          <Anchor
+            icon={<Linkedin size='medium' />}
+            href='https://www.linkedin.com/in/kishalozada/'
+          />
+          <Anchor icon={<Github />} href='https://github.com/kishaprudente' />
+          <Anchor
+            icon={<Instagram />}
+            href='https://www.instagram.com/kishamalzd/'
+          />
+          <Anchor icon={<Facebook />} href='https://www.facebook.com/kishaax' />
+          <Anchor icon={<Mail />} href='mailto:kisha.prudente@gmail.com' />
+        </StyledLinks>
+        <Link style={{ alignSelf: 'center' }} to='/projects'>
+          <ProjectButton size='large' primary>
+            Check My Projects
+          </ProjectButton>
+        </Link>
+      </Hero>
+      <About />
+      <Projects />
+    </div>
+  );
+};
 
 export default Home;
