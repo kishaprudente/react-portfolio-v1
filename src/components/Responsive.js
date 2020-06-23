1f9c25b82df62bc84660f861861518d9fa253c55
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-22 23:47:44 -0400

    created a responsive component

---
+++ src/components/Responsive.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Grid, ResponsiveContext } from 'grommet';
+
+const columns = {
+  small: ['auto'],
+  medium: ['auto', 'auto'],
+  large: ['auto', 'auto', 'auto'],
+  xlarge: ['auto', 'auto', 'auto'],
+};
+
+const rows = {
+  small: ['medium', 'medium', 'medium'],
+  medium: ['medium', 'medium'],
+  large: ['medium'],
+  xlarge: ['medium'],
+};
+
+const Responsive = ({
+  children,
+  overrideColumns,
+  overrideRows,
+  areas,
+  ...props
+}) => (
+  <ResponsiveContext.Consumer>
+    {(size) => {
+      let columnsVal = columns;
+      if (columns) {
+        if (columns[size]) {
+          columnsVal = columns[size];
+        }
+      }
+
+      let rowsVal = rows;
+      if (rows) {
+        if (rows[size]) {
+          rowsVal = rows[size];
+        }
+      }
+
+      let areasVal = areas;
+      if (areas && !Array.isArray(areas)) areasVal = areas[size];
+
+      return (
+        <Grid
+          {...props}
+          areas={!areasVal ? undefined : areasVal}
+          rows={!rowsVal ? size : rowsVal}
+          columns={!columnsVal ? size : columnsVal}
+        >
+          {children}
+        </Grid>
+      );
+    }}
+  </ResponsiveContext.Consumer>
+);
+
+export default Responsive;
